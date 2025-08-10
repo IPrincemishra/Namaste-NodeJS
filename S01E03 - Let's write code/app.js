91919e76f818c2818ed54908ddd30c00ddae0043
Prince Mishra <princemishra95927@gmail.com>
2025-08-10 23:27:36 +0530

    Namaste NodeJS

--- S01E03 - Let's write code/app.js
+++
@@ -1,9 +0,0 @@
-var name = "Namaste NodeJS"
-
-var a = 5;
-var b = 10;
-
-console.log(name);
-console.log(a + b);
-
-console.log(global);
---
+++ app.js
@@ -0,0 +1,9 @@
+var name = "Namaste NodeJS"
+
+var a = 7
+var b = 5
+
+// console.log(a + b);
+// console.log(name);
+
+console.log(globalThis === global);
